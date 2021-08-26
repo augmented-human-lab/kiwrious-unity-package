2432f6e8d89d98cda486b0c835ed2c2e98649c0f
bsc2fast <bsc2fast@hotmail.com>
2021-08-26 22:54:15 +0530

    initial commit

---
+++ Scripts/Runtime/IKiwriousReader.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Kiwrious.Scripts;
+
+namespace kiwrious {
+	public interface IKiwriousReader
+	{
+
+		SensorData GetConductivity();
+
+
+		SensorData GetVOC();
+
+
+		SensorData GetUVLux();
+
+
+		SensorData GetHumidityTemperature();
+
+
+		SensorData GetColor();
+
+
+		SensorData GetHeartRate();
+
+
+		SensorData GetBodyTemperature();
+
+
+		SensorData GetBodyTemperature2();
+
+	}
+}

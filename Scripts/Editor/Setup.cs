08c52cb97d97ea176ce352d7bfce7975c48e2197
bsc2fast <bsc2fast@hotmail.com>
2021-09-03 15:26:07 +0530

    auto update api compatibility level editor script

---
+++ Scripts/Editor/Setup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Startup
+{
+    [System.Obsolete]
+    static Startup()
+    {
+        PlayerSettings.apiCompatibilityLevel = ApiCompatibilityLevel.NET_4_6;
+    }
+}

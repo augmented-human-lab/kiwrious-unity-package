ba0aaa14ce6f84a8dad7a163340e583986e0d92b
bsc2fast <bsc2fast@hotmail.com>
2021-09-03 15:55:04 +0530

    api compatibility auto update for android

--- Scripts/Editor/Setup.cs
+++ Scripts/Editor/Setup.cs
@@ -7,5 +7,6 @@ public class Startup
     static Startup()
     {
         PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_4_6);
+        PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Android, ApiCompatibilityLevel.NET_4_6);
     }
 }

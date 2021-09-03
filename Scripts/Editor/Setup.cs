7cc3648647ba29b8704c500c8b63abeae32171cb
bsc2fast <bsc2fast@hotmail.com>
2021-09-03 15:47:34 +0530

    set api level update

--- Scripts/Editor/Setup.cs
+++ Scripts/Editor/Setup.cs
@@ -4,9 +4,8 @@ using UnityEditor;
 [InitializeOnLoad]
 public class Startup
 {
-    [System.Obsolete]
     static Startup()
     {
-        PlayerSettings.apiCompatibilityLevel = ApiCompatibilityLevel.NET_4_6;
+        PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_4_6);
     }
 }
